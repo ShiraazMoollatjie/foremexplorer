5241bc73d671303cd7088a6831562b6000bbbfc0
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-10-13 22:44:52 +0200

    pkg/ops: Add articles loop

---
+++ pkg/ops/forem.go
@@ -0,0 +1,31 @@
+package ops
+
+import (
+	"context"
+	"log"
+
+	"github.com/ShiraazMoollatjie/foremexplorer/pkg/db"
+	"github.com/ShiraazMoollatjie/foremexplorer/pkg/state"
+	"github.com/ShiraazMoollatjie/gophorem/pkg/gophorem"
+)
+
+func QueryForemArticlesForever(s *state.State) {
+	ctx := context.Background()
+	al, err := s.ForemClient.Articles(ctx, gophorem.Defaults())
+	if err != nil {
+		log.Printf("error querying dev.to: %+v", err)
+	}
+
+	for _, a := range al {
+		p := a.Article
+		p.TagList = a.TagList
+		p.Tags = a.Tags
+
+		id, err := db.AddArticle(s.DB, p)
+		if err != nil {
+			log.Printf("error saving article: %v\n article is: %+v", err, a)
+			continue
+		}
+		log.Println("saving article with id ", id)
+	}
+}
