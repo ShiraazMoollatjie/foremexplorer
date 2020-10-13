106e9b8a5a0285169d06f1da2f80af65f919b291
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-10-13 22:45:21 +0200

    foremexplorer: Update main program with loops

--- cmd/foremexplorer/main.go
+++ cmd/foremexplorer/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ShiraazMoollatjie/foremexplorer/pkg/db"
+	"github.com/ShiraazMoollatjie/foremexplorer/pkg/ops"
+	"github.com/ShiraazMoollatjie/foremexplorer/pkg/state"
 )
 
 func main() {
@@ -13,4 +19,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Cannot create db. Error: %+v", err)
 	}
+
+	s := state.NewState()
+	go ops.QueryForemArticlesForever(s)
+	waitForShutdown()
+}
+
+func waitForShutdown() {
+	termChan := make(chan os.Signal)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
+	<-termChan
+
+	fmt.Println("Shutting down")
 }
