53ebad9c1145cbf942bed31d0fd2738628bc1195
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-10-19 22:20:57 +0200

    pkg/ops: Add an article ops function

--- pkg/ops/forem.go
+++ pkg/ops/forem.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ShiraazMoollatjie/foremexplorer/pkg/db"
 	"github.com/ShiraazMoollatjie/foremexplorer/pkg/state"
 	"github.com/ShiraazMoollatjie/gophorem/pkg/gophorem"
+	"github.com/jinzhu/gorm"
 )
 
 func QueryForemArticlesForever(s *state.State) {
@@ -21,11 +22,23 @@ func QueryForemArticlesForever(s *state.State) {
 		p.TagList = a.TagList
 		p.Tags = a.Tags
 
-		id, err := db.AddArticle(s.DB, p)
+		id, err := AddArticle(ctx, s, p)
 		if err != nil {
-			log.Printf("error saving article: %v", err, a)
+			log.Printf("error saving article: %+v", err)
 			continue
 		}
 		log.Println("saving article with id ", id)
 	}
+}
+
+func AddArticle(ctx context.Context, s *state.State, article gophorem.Article) (uint, error) {
+	a, err := db.LookupByDevToID(s.DB, article.ID)
+	if err != nil && !gorm.IsRecordNotFoundError(err) {
+		return 0, err
+	} else if err != nil && gorm.IsRecordNotFoundError(err) {
+		return db.AddArticle(s.DB, article)
+	}
+
+	// Found article, so can just return the id.
+	return a.ID, nil
 }
