dc2397c798e7822f84e6fd28bbc1e05e1d3d79bf
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-10-19 22:44:30 +0200

    pjg/ops: Timing the imports

--- pkg/ops/dataseed.go
+++ pkg/ops/dataseed.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"strconv"
+	"time"
 
 	"github.com/ShiraazMoollatjie/foremexplorer/pkg/state"
 	"github.com/ShiraazMoollatjie/gophorem/pkg/gophorem"
@@ -16,7 +17,7 @@ func SeedDevData(s *state.State) {
 
 	pg := 1
 	for {
-
+		log.Printf("importing page number %d", pg)
 		al, err := s.ForemClient.Articles(ctx, gophorem.Arguments{
 			"page":     strconv.Itoa(pg),
 			"per_page": strconv.Itoa(pageSize),
@@ -24,14 +25,13 @@ func SeedDevData(s *state.State) {
 
 		if err != nil {
 			log.Printf("cannot fetch forem articles: %v", err)
+			continue // retry
 		}
 
-		for _, a := range al {
-			at := a.Article
-			at.TagList = a.TagList
-			at.Tags = a.Tags
-
-			AddArticle(ctx, s, at)
+		err = importPage(ctx, s, al)
+		if err != nil {
+			log.Printf("cannot import forem articles: %v", err)
+			continue // retry
 		}
 
 		// Assume all pages equal 1000 until you reach the end.
@@ -40,5 +40,22 @@ func SeedDevData(s *state.State) {
 		}
 		pg++
 	}
+}
 
+func importPage(ctx context.Context, s *state.State, al gophorem.Articles) error {
+	t0 := time.Now()
+	defer func() { log.Printf("time taken to import page %s seconds", time.Since(t0)) }()
+
+	for _, a := range al {
+		at := a.Article
+		at.TagList = a.TagList
+		at.Tags = a.Tags
+
+		_, err := AddArticle(ctx, s, at)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
